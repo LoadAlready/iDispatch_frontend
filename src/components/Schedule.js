2878f61bf00858cb12240941f462c2bc548f5054
Avi Cohen <github email address>
2018-08-13 21:30:05 -0400

    working sort on both previous and upcoming job components

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import UUID from 'uuid'
+import { debug } from 'util';
 
 export default class Schedule extends Component {
 
     mapNextFiveJobs = () => {
-        let newDate = null;
         let futureAppointments = this.props.jobs.filter((job) => {
             let timeArray = job.schedule_time.split(':');
             var newTime = new Date(String(job.schedule_date_year),
@@ -12,13 +12,28 @@ export default class Schedule extends Component {
                 String(job.schedule_date_day),
                 timeArray[0],
                 timeArray[1]);
-            newDate = newTime.toUTCString().split(' ').slice(0, 5).join(' ');
             return Date.parse(newTime) > Date.now()
         })
+        let sortedFutureAppointments = futureAppointments.sort((a, b) => {
+            let aTimeArray = a.schedule_time.split(':');
+            let bTimeArray = b.schedule_time.split(':');
 
+            let aDate = new Date(String(a.schedule_date_year),
+                String(a.schedule_date_month),
+                String(a.schedule_date_day),
+                aTimeArray[0],
+                aTimeArray[1]);
+            let bDate = new Date(String(b.schedule_date_year),
+                String(b.schedule_date_month),
+                String(b.schedule_date_day),
+                bTimeArray[0],
+                bTimeArray[1]);    
+            return aDate - bDate
+        });
 
-        return futureAppointments.slice(0, 5).map((job) => {
-            return <li key={UUID()} className="padding-bottom"><strong>Job Number:</strong> {job.job_number} <br /> {newDate}</li>
+        return sortedFutureAppointments.slice(0, 5).map((job) => {
+            let shortenedDate = new Date(job.schedule_date_year, job.schedule_date_month, job.schedule_date_day);
+            return <li key={UUID()} className="padding-bottom"><strong>Job Number:</strong> {job.job_number} <br />{shortenedDate.toDateString()}</li>
         })
     }
 
