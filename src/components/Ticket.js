652b527d2219fdc869f84cebc1d8009fd2ef08f8
loadalready <justaviram@gmail.com>
2018-08-16 11:10:45 -0400

    refactoring ticket component into class and turn form into controlled form. working on posting back to db

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -3,24 +3,144 @@ import { Form } from 'semantic-ui-react'
 
 
 class Ticket extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      clientName: "",
+      requestedBy: "",
+      jobNumber: "",
+      month: "",
+      day: "",
+      year: "",
+      leadTech: "",
+      timeIn: "",
+      timeOut: "",
+      secondaryTechs: "",
+      materialPos: "",
+      suppliers: "",
+      jobDescription: "",
+      jobNotes: "",
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  
   handleSubmit = (event) => {
     debugger
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
     return (
       <Form onSubmit={(event) => this.handleSubmit(event)}>
         <Form.Group>
-          <Form.Input label='Client Name' placeholder='Client Name' width={4} />
-          <Form.Input label='Requested By' placeholder='Requested By' width={4} />
-          <Form.Input label='Job Number' placeholder='Job Number' width={2} />
-          <Form.Input label='Schedule Date' placeholder='Schedule Date' width={2} />
-          <Form.Input label='Technician Name' placeholder='Technician Name' width={4} />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="clientName"
+            label='Client Name' 
+            placeholder='Client Name' 
+            width={4}
+            value={this.state.clientName} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="requestedBy"
+            label='Requested By' 
+            placeholder='Requested By' 
+            width={3}
+            value={this.state.requestedBy} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="jobNumber"
+            label='Job Number' 
+            placeholder='Job Number' 
+            width={2}
+            value={this.state.jobNumber} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="month"
+            label='Month' 
+            placeholder='Month' 
+            width={1}
+            value={this.state.month} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="day"
+            label='Day' 
+            placeholder='Day' 
+            width={1}
+            value={this.state.day} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="year"
+            label='Year' 
+            placeholder='Year' 
+            width={1}
+            value={this.state.year} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="leadTech"
+            label='Lead Technician' 
+            placeholder='Lead Technician' 
+            width={4}
+            value={this.state.leadTech} 
+          />
         </Form.Group>
         <Form.Group>
-          <Form.Input label='Street Address' placeholder='Street Address' width={6} />
-          <Form.Input label='City, State, Zip' placeholder='City, State, Zip' width={6} />
-          <Form.Input label='Phone' placeholder='Phone' width={4} />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="timeIn"
+            label='Time In' 
+            placeholder='Time In' 
+            width={2}
+            value={this.state.timeIn} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="timeOut"
+            label='Time Out' 
+            placeholder='Time Out' 
+            width={2}
+            value={this.state.timeOut} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="secondaryTechs"
+            label='Addional Techs' 
+            placeholder='Addional Techs' 
+            width={4}
+            value={this.state.secondaryTechs} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="materialPos"
+            label='Material POs' 
+            placeholder='Material POs' 
+            width={4}
+            value={this.state.materialPos} 
+          />
+          <Form.Input
+            onChange={this.handleInputChange} 
+            name="suppliers"
+            label='Supplier ID' 
+            placeholder='Supplier ID' 
+            width={4}
+            value={this.state.suppliers} 
+          />
         </Form.Group>
         <Form.Group>
           <Form.TextArea label='Job Description' placeholder='Description' width={8} />
